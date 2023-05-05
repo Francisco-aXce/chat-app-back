93067ef0672482ae90c4b712b2ffe3a88eed1cb5
Francisco Ramos <rfrancisco.dev@gmail.com>
2023-05-05 09:42:30 -0300

    add token validation

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 /**
  * Check if the user already exists in the database, if so, return a 403 status code.
@@ -34,7 +35,35 @@ const validSignin = async (req, res, next) => {
     }
 };
 
+/**
+ * Check if the token is valid, and if the user exists.
+ * 
+ */
+const tokenValidate = async (req, res, next) => {
+    try {
+        const token = req.headers['x-access-token'];
+
+        if(!token) {
+            return res.status(403).json({ message: 'No token provided' });
+        }
+
+        const decoded = jwt.verify(token, process.env.SECRET);
+        res.locals.user = decoded;
+        const userFound = await User.findById(decoded.id).select('-password');
+
+        if(!userFound) {
+            return res.status(404).json({ message: 'No do not exists' });
+        }
+
+        next();   
+    } catch (error) {
+        // Unauthorized
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+};
+
 module.exports = {
     validSignup,
     validSignin,
+    tokenValidate,
 };
